21189b407aea2e53a7b6685360b14adf7e03cebb
Sergio Reverte <129745342+Reverte10@users.noreply.github.com>
2023-06-25 09:23:15 +0000

    final commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [colorRed, setColorRed] = useState("danger")
+	const [colorOrange, setColorOrange] = useState("warning")
+	const [colorGreen, setColorGreen] = useState("success")
+
+	const changeState1 = () => {
+		if(colorRed === "danger"){
+			setColorRed("danger glow")
+			setColorOrange("warning")
+			setColorGreen("success")
+		}
+	}
+
+	const changeState2 = () => {
+		if(colorOrange === "warning"){
+			setColorOrange("warning glow")
+			setColorRed("danger")
+			setColorGreen("success")
+		} 
+	}
+
+	const changeState3 = () => {
+		if(colorGreen === "success"){
+			setColorGreen("success glow")
+			setColorRed("danger")
+			setColorOrange("warning")
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<article className="text-center">
+			<div className={"rounded bg-black mx-auto"} style={{width:"12px", height:"100px"}}></div>
+			<div className="rounded bg-black p-3 mx-auto" style={{width:"80px", height:"200px"}}>
+				<div className={"rounded-circle m-1 bg-" + colorRed} style={{width:"50px", height:"50px"}} onClick={changeState1}></div>
+				<div className={"rounded-circle m-1 bg-" + colorOrange} style={{width:"50px", height:"50px"}} onClick={changeState2}></div>
+				<div className={"rounded-circle m-1  bg-" + colorGreen} style={{width:"50px", height:"50px"}} onClick={changeState3}></div>
+			</div>
+		</article>
 	);
 };
 
